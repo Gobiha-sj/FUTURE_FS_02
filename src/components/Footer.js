c7337b09919e91a8dd0de4a0b01132d57e51c6b2
Gobiha Selvam <gobihasj12344@gmail.com>
2026-06-18 21:58:22 +0530

    Add files via upload
    Added frontend files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-light text-center py-3 mt-4 shadow-sm">
+      <p className="mb-0">© 2026 Gobiha CRM </p>
+    </footer>
+  );
+}
+
+export default Footer;
